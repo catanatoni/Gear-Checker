688cb3c8f06eb9013e1f8eedf6e2ce00c9e2009b
catanatoni <tonii_96@me.com>
2026-08-13 15:40:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-const CACHE='gear-check-v2.2';const FILES=['./','./index.html','./styles.css','./fixes.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+const CACHE = 'gear-check-v1-recovered-icons-2';
+const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './icons/cases/VevorMediumStyled.png', './icons/cases/VevorSmallStyled.png', './icons/cases/VevorLargeStyled.png', './icons/cases/ManfrottoRollerStyled.png', './icons/cases/CameraShoulderBag.png', './icons/cases/HardcaseCompact.png', './icons/cases/HardcaseMedium.png', './icons/cases/HardcaseLarge.png', './icons/cases/HardcaseLong.png', './icons/cases/HardcaseLens.png', './icons/cases/HardcaseBattery.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
